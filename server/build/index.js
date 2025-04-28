f07446d907fed223c7d5d4f16fc466e4cc4015d5
khinenweoo <devkhinenweoo@gmail.com>
2025-04-28 09:55:17 +0700

    fix: cors allow from any origin

--- server/build/index.js
+++ server/build/index.js
@@ -27,13 +27,13 @@ const handleServerError = (error) => {
 };
 const mongoURI = process.env.MONGO_URI || "http://localhost:3000/";
 // const originURL: string = process.env.FRONTEND_URL || "";
-const corsOptions = {
-    origin: 'https://personal-finance-tracker-sage-alpha.vercel.app', // Only allow this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-};
+// const corsOptions = {
+//     origin: originURL, // Replace with your frontend URL
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+// };
 app.use(express_1.default.json());
-app.use((0, cors_1.default)(corsOptions));
+app.use((0, cors_1.default)());
 mongoose_1.default.connect(mongoURI).then(() => console.log("CONNECTED TO MONGODB!")).catch((err) => console.error("Failed to Connect to MongoDB:", err));
 app.use("/financial-records", financial_records_1.default);
 app.listen(port, () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -24,14 +24,14 @@ const handleServerError = (error: any) => {
 
 const mongoURI: string = process.env.MONGO_URI || "http://localhost:3000/";
 // const originURL: string = process.env.FRONTEND_URL || "";
-const corsOptions = {
-    origin: 'https://personal-finance-tracker-sage-alpha.vercel.app', // Only allow this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-};
+// const corsOptions = {
+//     origin: originURL, // Replace with your frontend URL
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+// };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 mongoose.connect(mongoURI).then(() => console.log("CONNECTED TO MONGODB!")).catch((err) => console.error("Failed to Connect to MongoDB:", err));
